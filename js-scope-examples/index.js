0057713345b1c4dcf31802b41dca564679039903
Rigo-codes <rodrigo.s.m.jr@gmail.com>
2024-11-06 22:10:29 -0800

    pushhhhhh

---
+++ js-scope-examples/index.js
@@ -0,0 +1,44 @@
+// let globalVar = "I am a variable!";
+
+// console.log("Accessing globalvar in the global scope.", globalVar);
+
+// if (true) {
+//     console.log("Accessing globalVar inside of a block.", globalVar);
+
+// }
+
+// if (true) {
+//     let localVar = "I am a local variable!";
+//     console.log("Accessing localVar inside of the block.", localVar);
+// }
+
+// console.log("Accessing localVar outside of the block.", localVar);
+
+// let message = "global message.";
+
+// if (true) {
+//     let message = "Local message.";
+//     console.log("Accessing message inside of the block.", message);
+
+// }
+
+// console.log("Accessing message in the global scope.", message);
+
+// let counter = 0;
+
+// if (true) {
+//     counter = 5;
+//     console.log("Modified counter inside od the block.", counter);
+
+// }
+
+// console.log("Accessing modified counter in the global scope.", counter);
+
+if (true) {
+    let counter = 10;
+    console.log("Counter inside the block.", counter);
+
+
+}
+
+counter = counter + 1;
